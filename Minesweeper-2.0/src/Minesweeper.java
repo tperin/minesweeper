a2b46ab07fb148a2573ee7b5dd5d5e9462416725
tperin <tperin808@gmail.com>
2013-04-02 14:17:59 -0400

    added random board generation

--- Minesweeper-2.0/src/Minesweeper.java
+++ Minesweeper-2.0/src/Minesweeper.java
@@ -14,5 +14,30 @@ public class Minesweeper {
 	public Minesweeper(int x, int y) {
 		
 	}
+	
+	/**
+	 * Generates a random board
+	 * @param x number of rows
+	 * @param y number of columns
+	 * @param pr probability of bombs, given as number between 1 and 100
+	 * @return the generated board
+	 */
+	public static char[][] randBoard(int x, int y, int pr) {
+		char[][] board = new char[x][y];
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				//chance of bomb will be 1/n
+				double p = pr;
+				double r = Math.round(Math.random()*100);
+				//if (DEBUG) System.out.println(r);
+				if (r <= p) {
+					board[i][j] = 'B';
+				}
+				else board[i][j] = 'U';
+			}
+		}
+		
+		return board;
+	}
 
 }
