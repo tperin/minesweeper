faabfb1f7ebe31ed0779ea5feb44cd5f89a5857a
tperin <tperin808@gmail.com>
2013-04-02 14:15:39 -0400

    added instance variables

--- Minesweeper-2.0/src/Minesweeper.java
+++ Minesweeper-2.0/src/Minesweeper.java
@@ -1,12 +1,18 @@
 
 public class Minesweeper {
-
+	private char[][] board;
+	private boolean revealBoard;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public Minesweeper(int x, int y) {
+		
 	}
 
 }
